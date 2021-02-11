e60ae891231525714073e93be17cf9230a9eb53f
Russ Hughes <russ@owt.com>
2021-02-10 22:43:36 -0800

    Removed cyrilc to recover some flash

--- examples/hershey.py
+++ examples/hershey.py
@@ -11,7 +11,6 @@ import random
 
 # Load several frozen fonts from flash
 
-import cyrilc
 import gotheng
 import greeks
 import italicc
@@ -45,10 +44,8 @@ def cycle(p):
 COLORS = cycle([0xe000, 0xece0, 0xe7e0, 0x5e0, 0x00d3, 0x7030])
 
 FONTS = cycle([
-    cyrilc, gotheng, greeks, italicc,
-    italiccs, meteo, music, romanc,
-    romancs, romand, romanp, romans,
-    romant, scriptc, scripts])
+    gotheng, greeks, italicc, italiccs, meteo, music, romanc,
+    romancs, romand, romanp, romans, romant, scriptc, scripts])
 
 GREETINGS = cycle([
     "bonjour", "buenas noches", "buenos dias",
